682dfb72a3743933313fb60af773f2d5ec87d185
bingmani0922 <95996860+bingmani0922@users.noreply.github.com>
2024-10-24 07:41:55 +0000

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,12 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_models import ChatOpenAI
 
 
 # 환경 변수 설정
-api_key = st.secrets["OPENAI_API_KEY"]
-os.environ["OPENAI_API_KEY"] = api_key
+# api_key = st.secrets["OPENAI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = "sk-Ff5UtnaNduMinWUeVMQIniOiThvc9jnnzmUp1Wsx_JT3BlbkFJqsndc9ZGaNBv7pT9CepAZfdSUhSCTqAhMK1B2nL8cA"
 
 # 메모리 객체 생성
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
